55ef7a053aa02f3c8d092168a7ce95d9373497a5
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2017-07-31 21:01:13 -0300

    Refactor freight function

--- functions/index.js
+++ functions/index.js
@@ -1,38 +1,55 @@
 const functions = require('firebase-functions')
-const axios = require('axios')
-const qs = require('qs')
+const request = require('request')
 const xml = require('xml2js')
+
+const mapResults = results => {
+  return results['Servicos']['cServico']
+    .map(freight => ({
+      code: freight['Codigo'][0],
+      price: +(freight['Valor'][0].replace(',', '.') || 0),
+      delivery: {
+        time: +(freight['PrazoEntrega'][0] || 0),
+        home: freight['EntregaDomiciliar'][0] === 'S',
+        onSaturday: freight['EntregaSabado'][0] === 'S',
+        restriction: freight['MsgErro'][0]
+      }
+    }))
+    .filter(freight => !!freight.price)
+}
 
 const calcFreight = code => {
   const url = 'http://ws.correios.com.br/calculador/CalcPrecoPrazo.aspx'
 
   const parser = new xml.Parser()
 
-  const data = qs.stringify({
-    nCdEmpresa: '',
-    sDsSenha: '',
-    nCdServico: '40010,40045,40215,40290,41106',
-    sCepOrigem: '02998060',
-    sCepDestino: code,
-    nVlPeso: 0.800,
-    nCdFormato: 1,
-    nVlComprimento: 18,
-    nVlAltura: 9,
-    nVlLargura: 13.5,
-    nVlDiametro: 0,
-    sCdMaoPropria: 'N',
-    nVlValorDeclarado: 0,
-    sCdAvisoRecebimento: 'S',
-    StrRetorno: 'XML',
-    sDtCalculo: '3'
-  })
+  return new Promise((resolve, reject) => {
+    const data = {
+      nCdEmpresa: '',
+      sDsSenha: '',
+      sCepOrigem: '70002900',
+      sCepDestino: code,
+      nVlPeso: 0.800,
+      nCdFormato: 1,
+      nVlComprimento: 18,
+      nVlAltura: 9,
+      nVlLargura: 13.5,
+      sCdMaoPropria: 'n',
+      nVlValorDeclarado: 0,
+      sCdAvisoRecebimento: 's',
+      nCdServico: '40010,40045,40215,40290,41106',
+      nVlDiametro: 0,
+      StrRetorno: 'xml',
+      nIndicaCalculo: '3'
+    }
 
-  return axios.post(url, data).then(({ data }) => {
-    return new Promise((resolve, reject) => {
-      parser.parseString(data, (err, json) => {
+    request.get(url, { qs: data }, (err, response) => {
+      if (err)
+        reject('Erro no serviço online dos correios.')
+
+      parser.parseString(response.body, (err, results) => {
         if (err)
-          reject(err)
-        resolve(json)
+          reject('Erro ao traduzir os dados dos correios.')
+        resolve(mapResults(results))
       })
     })
   })
@@ -47,6 +64,7 @@ exports.freight = functions.https.onRequest((request, response) => {
   calcFreight(code)
     .then(data => response.send(data))
     .catch(err => {
-      response.send(500)
+      const message = typeof err === 'string' ? err : 'Erro ao calcular o frete.'
+      response.send(500, message)
     })
 });

19aaadedbe65a3bcfc3d3a150943d4ad235f584d
Vitor Cavalcanti <vitorluizc@outlook.com>
2017-08-06 16:09:45 -0300

    Fix response charset

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,7 @@
-const functions = require('firebase-functions')
+const { Parser } = require('xml2js')
+const { Iconv } = require('iconv')
 const request = require('request')
-const xml = require('xml2js')
+const functions = require('firebase-functions')
 
 const mapResults = results => {
   const names = {
@@ -29,33 +30,38 @@ const mapResults = results => {
 const calcFreight = code => {
   const url = 'http://ws.correios.com.br/calculador/CalcPrecoPrazo.aspx'
 
-  const parser = new xml.Parser()
+  const parser = new Parser()
 
   return new Promise((resolve, reject) => {
-    const data = {
-      nCdEmpresa: '',
-      sDsSenha: '',
-      sCepOrigem: '70002900',
-      sCepDestino: code,
-      nVlPeso: 0.800,
-      nCdFormato: 1,
-      nVlComprimento: 18,
-      nVlAltura: 9,
-      nVlLargura: 13.5,
-      sCdMaoPropria: 'n',
-      nVlValorDeclarado: 0,
-      sCdAvisoRecebimento: 's',
-      nCdServico: '40010,40045,40215,40290,41106',
-      nVlDiametro: 0,
-      StrRetorno: 'xml',
-      nIndicaCalculo: '3'
+    const options = {
+      qs: {
+        nCdEmpresa: '',
+        sDsSenha: '',
+        sCepOrigem: '70002900',
+        sCepDestino: code,
+        nVlPeso: 0.800,
+        nCdFormato: 1,
+        nVlComprimento: 18,
+        nVlAltura: 9,
+        nVlLargura: 13.5,
+        sCdMaoPropria: 'n',
+        nVlValorDeclarado: 0,
+        sCdAvisoRecebimento: 's',
+        nCdServico: '40010,40045,40215,40290,41106',
+        nVlDiametro: 0,
+        StrRetorno: 'xml',
+        nIndicaCalculo: '3'
+      },
+      encoding: null
     }
 
-    request.get(url, { qs: data }, (err, response) => {
+    request.get(url, options, (err, response) => {
       if (err)
         reject('Erro no serviço online dos correios.')
 
-      parser.parseString(response.body, (err, results) => {
+      const buffer = new Iconv('ISO-8859-1', 'UTF-8').convert(response.body)
+
+      parser.parseString(buffer.toString(), (err, results) => {
         if (err)
           reject('Erro ao traduzir os dados dos correios.')
         resolve(mapResults(results))
@@ -65,15 +71,17 @@ const calcFreight = code => {
 }
 
 exports.freight = functions.https.onRequest((request, response) => {
-  const code = request.body.code
+  const isCode = /^\d{8}$/
 
-  if (!code)
-    response.send(500, 'Parâmetro incorreto ou vazio.')
+  if (!isCode.test(request.body.code)) {
+    response.status(500).send('CEP incorreto ou vazio.')
+    return
+  }
 
-  calcFreight(code)
+  calcFreight(request.body.code)
     .then(data => response.send(data))
     .catch(err => {
       const message = typeof err === 'string' ? err : 'Erro ao calcular o frete.'
-      response.send(500, message)
+      response.status(500).send(message)
     })
-});
+})
